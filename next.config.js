758f38df6ab48acdd3d1e376a11f922f36869a4f
Saiful Islam <islam.saiful@selisegroup.com>
2025-09-12 04:30:55 +0600

    Git deployment flow added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+    output: 'export',   // 👈 tells Next.js to generate static files
+    images: {
+        unoptimized: true, // GitHub Pages can't handle Next.js Image optimization
+    },
+    basePath: '/kali-o-kobita', // if not deploying at root
+    assetPrefix: '/kali-o-kobita/', // important for assets
 }
 
 module.exports = nextConfig

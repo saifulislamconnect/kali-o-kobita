3a140fae53bc81ec2fd98d6feafdb6d377d4e8e4
Saiful Islam <saifulislam.connect@gmail.com>
2026-06-24 03:52:47 +0600

    added adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Bengali:wght@400;700&display=swap" rel="stylesheet" />
         <title>কালি ও কবিতা - একটি বাংলা প্রেমের প্রচেষ্টা</title>
       </Head>
+      <Script 
+        async 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3911519177971711"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
       <Component {...pageProps} />
     </>
   );
